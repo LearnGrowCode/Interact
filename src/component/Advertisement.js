d8c13c2c12a076dd98ed05e9a8fa64bab42928d5
Akshay A Kalekar <50509605+akshay-Kalekar@users.noreply.github.com>
2022-09-02 12:16:58 +0530

    Add files via upload

---
+++ src/component/Advertisement.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function AdvertisementPoster(){
+    
+return (
+    <div className="flex justify-center w-full  "> 
+    <img src="Poster.png" className=" w-60  py-2  sm:w-[80vw] sm:h-max-[30vh] "/>
+    </div>
+)
+}
